d9e9e191820c180692dfa46c6b81ecc2d620c6c2
william8622 <zhangqiang8622@gmail.com>
2021-09-26 11:47:56 +0900

    init2

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const cors = require("cors")
 const port = process.env.PORT || 5000
 const authRoutes = require("./routes/authRoutes")
 
 // middlewares
+app.use(cors())
 app.use(express.json())
 
 // routes
 app.use("/auth",authRoutes)
+app.use("/",(req,res)=> res.send("hello world"))
 
 mongoose.connect(process.env.DB_CONNECT)
     .catch(err => console.error(err))
